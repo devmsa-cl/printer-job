e556d281466b01385695702674733c2fc0def52a
ilormsa <chercllor@gmail.com>
2022-10-09 20:13:44 -0700

    update files

---
+++ index.js
@@ -0,0 +1,60 @@
+const { print, getPrinters, getDefaultPrinter } = require("unix-print");
+const express = require("express");
+require("dotenv").config();
+const cronJob = require("cron").CronJob;
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+let printFiles;
+let index = 0;
+let options = ["-o fit-to-page", "-o media=Letter", "-q 100"]; //
+
+const dir = path.resolve("files");
+
+// reading all the files in the files folder
+fs.readdir(dir, (err, files) => {
+  if (err) return console.log(err);
+
+  printFiles = files;
+
+  //   printJob();
+});
+
+const job = new cronJob(
+  "1/1 * * * *",
+  //   process.env.CRON_JOB,
+  function () {
+    PrintFile();
+    // printJob();
+  },
+  null,
+  true,
+  process.env.CRON_TIMEZONE
+);
+
+job.start();
+
+function PrintFile() {
+  console.log(printFiles[index]);
+  getNextFileReady();
+}
+
+// update the index and get the next file to ready to print
+function getNextFileReady() {
+  index++;
+  if (index >= printFiles.length) index = 0;
+}
+
+function printJob() {
+  print(path.join(dir, printFiles[index]), process.env.PRINTER_NAME, options)
+    .then((res) => {
+      console.log("Printer has started");
+      getNextFileReady();
+    })
+    .catch((err) => console.log(err));
+}
+
+app.listen(5921, function () {
+  console.log("Printer cron job started on port 5921");
+});
